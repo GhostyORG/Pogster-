80a4f35ee7665fca5f054c8da427477dbf33a126
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-07-27 03:41:08 -0400

    Add files via upload
    A clone of pogy, but written by myself.

---
+++ utils/variables.js
@@ -0,0 +1,21 @@
+const config = require("./../config.json");
+let token;
+let mongodb;
+
+if (config.dev) {
+    token = config.main_token;
+    mongodb = config.mongodb_url;
+}
+if (!config.dev) {
+    token = config.main_token
+    mongodb = config.mongodb_url
+
+};
+
+
+
+module.exports = {
+    config,
+    token,
+    mongodb
+};
